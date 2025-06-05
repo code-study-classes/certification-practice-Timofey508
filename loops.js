4d8c8dde231b15c6519047f51f41c924d8e592f2
Ваше Имя <you@example.com>
2025-06-05 17:32:16 +0300

    sfs

--- loops.js
+++ loops.js
@@ -11,7 +11,28 @@ function sumOfSquares (a,b) {
 const power = (a,n) =>{
     const nums = Array.from(`${a}`.repeat(n));
     return nums.reduce((pow,num)=>pow *= parseInt(num, 10),1)
-    
-    console.log(nums);
-
 }
+//=====================================================================================
+// function findMax (arr){
+//     if (arr.length === 0)
+//         return [];
+//     let max = arr[0];
+//     for (let i = 1; i < arr.length; i++){
+//         if (arr[i] > max){
+//             max = arr[i];
+//         }
+//     }
+//     return max;
+// }
+// const findMMax = (nums) => nums.reduse((mx, num) => num > mx? num : mx, -Infinity);
+// _.max(nums) || [];
+//======================================================================================
+// const calculateAverage = () =>
+//     (nums.reduce((sum,num) => sum += num, 0) / nums.length) || [];
+//======================================================================================
+// function  getEvenNumbers2 (arr) {
+//     return arr.filter(num => num % 2 === 0);
+// }
+//======================================================================================
+// function getIntersection = (str1,str2) => str1.filter((num) => str2.inckudes)
+//======================================================================================
